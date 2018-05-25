cbe2d34220c6072b026d48cbd4643dcfc5f88595
elenaJO <ele.lucero1993@gmail.com>
2018-05-25 15:53:48 -0500

    integracion barra lateral

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,53 +1,59 @@
 <template>
  	<v-container :fluid="true" pl-0 pt-0 pr-0 pb-0>
 	 	<v-layout row wrap>
-		 	<v-flex xs6 md3>
-			<v-navigation-drawer
-    		stateless
-    		value="true"
-				class="blue lighten-3 height"
-  		>
-    		<v-list>
-      		<v-list-tile>
-        		<v-list-tile-action>
-          		<v-icon>home</v-icon>
-        		</v-list-tile-action>
-        	  <v-list-tile-title>Item1</v-list-tile-title>
-      		</v-list-tile>
-      		<v-list-group
-        		prepend-icon="account_circle"
-        		value="true"
-      		>
-        		<v-list-tile slot="activator">
-          		<v-list-tile-title>Item2</v-list-tile-title>
-        		</v-list-tile>
-       			 	<v-list-group
-          			sub-group
-          			no-action
-          			value="true"
-        				>
-          				<v-list-tile slot="activator">
-            				<v-list-tile-title>sub-1</v-list-tile-title>
-          				</v-list-tile>
-        			</v-list-group>
-        			<v-list-group
-          			sub-group
-          			no-action
-        			>
-          				<v-list-tile slot="activator">
-            				<v-list-tile-title>sub-2</v-list-tile-title>
-          				</v-list-tile>
-        			</v-list-group>
-     			 </v-list-group>
-    		</v-list>
-  		</v-navigation-drawer>
-		 </v-flex>	
-	 </v-layout>	 
+		 	<v-flex xs8 md3>
+			 <v-navigation-drawer stateless value="true" class="blue lighten-3 height">
+    			<v-list v-for="result in results" :key="result.id">
+			 			<v-list-group value="true">
+        			<v-list-tile slot="activator">
+								<img class="icon" :src="result.pathIcon">
+          			<v-list-tile-title>{{result.name}}</v-list-tile-title>
+        			</v-list-tile>
+       				<v-list-tile v-for="resultlist in result.resource.data" :key="resultlist.position">
+            		<v-list-tile-title class="sub-item">{{resultlist.name}}</v-list-tile-title>
+	              <img :src="resultlist.iconUrl" class="icon">
+							</v-list-tile>
+      			</v-list-group>
+    			</v-list>
+  			</v-navigation-drawer>
+		 	</v-flex>	
+	 	</v-layout>	 
  </v-container>
 </template>
+
+<script>
+async function created() {
+	const url = 'sidebar/git';
+	const token = localStorage.getItem('token');
+	const response = await this.$http.get(url, {
+		headers: { Authorization: `Bearer ${token}` },
+	});
+	this.results = response.data.data;
+}
+
+function data() {
+	return {
+		results: [],
+	};
+}
+export default {
+	data,
+	created,
+};
+</script>
 
 <style lang="scss" scoped>
   .height {
 		height: 100vh !important;
+	}
+
+	.icon {
+		height: 20px;
+		margin-right: 10px;
+		width: 20px;
+	}
+
+	.sub-item {
+		margin-left: 30px;
 	}
 </style>
