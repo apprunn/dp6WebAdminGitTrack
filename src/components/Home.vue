6ef98bd70990393a20120c8cf8b86fbc68b8e741
elenaJO <ele.lucero1993@gmail.com>
2018-05-25 12:46:34 -0500

    correccion

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,53 @@
 <template>
-	<div class="hello">
-		<h1>Home</h1>
-	</div>
+ 	<v-container :fluid="true" pl-0 pt-0 pr-0 pb-0>
+	 	<v-layout row wrap>
+		 	<v-flex xs6 md3>
+			<v-navigation-drawer
+    		stateless
+    		value="true"
+				class="blue lighten-3 height"
+  		>
+    		<v-list>
+      		<v-list-tile>
+        		<v-list-tile-action>
+          		<v-icon>home</v-icon>
+        		</v-list-tile-action>
+        	  <v-list-tile-title>Item1</v-list-tile-title>
+      		</v-list-tile>
+      		<v-list-group
+        		prepend-icon="account_circle"
+        		value="true"
+      		>
+        		<v-list-tile slot="activator">
+          		<v-list-tile-title>Item2</v-list-tile-title>
+        		</v-list-tile>
+       			 	<v-list-group
+          			sub-group
+          			no-action
+          			value="true"
+        				>
+          				<v-list-tile slot="activator">
+            				<v-list-tile-title>sub-1</v-list-tile-title>
+          				</v-list-tile>
+        			</v-list-group>
+        			<v-list-group
+          			sub-group
+          			no-action
+        			>
+          				<v-list-tile slot="activator">
+            				<v-list-tile-title>sub-2</v-list-tile-title>
+          				</v-list-tile>
+        			</v-list-group>
+     			 </v-list-group>
+    		</v-list>
+  		</v-navigation-drawer>
+		 </v-flex>	
+	 </v-layout>	 
+ </v-container>
 </template>
 
-<script>
-export default {
-	name: 'home',
-	data() {
-		return {
-			msg: 'Welcome to Your Vue.js PWA',
-		};
-	},
-};
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-h1 {
-	color: map-get($colors, red);
-	font-size: em(32);
-}
+  .height {
+		height: 100vh !important;
+	}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
 import login from '@/components/login';
-import sidebar from '@/components/sidebar';
 
 Vue.use(Router);
 
@@ -18,11 +17,6 @@ export default new Router({
 			path: '/login',
 			name: 'login',
 			component: login,
-		},
-		{
-			path: '/sidebar',
-			name: 'sidebar',
-			component: sidebar,
 		},
 	],
 });
