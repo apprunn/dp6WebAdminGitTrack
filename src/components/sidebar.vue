0a54f283af6ee41af0e5496ae59a2074f83b59c3
elenaJO <ele.lucero1993@gmail.com>
2018-05-25 11:43:04 -0500

    maquetacion barra lateral

---
+++ src/components/sidebar.vue
@@ -0,0 +1,71 @@
+<template>
+ <v-container :fluid="true" pl-0 pt-0 pr-0 pb-0>
+	 <v-layout row wrap>
+		 <v-flex xs6 md3>
+			  <v-navigation-drawer
+    stateless
+    value="true"
+		class="blue lighten-3 height"
+  >
+    <v-list>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>home</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-title>Item1</v-list-tile-title>
+      </v-list-tile>
+      <v-list-group
+        prepend-icon="account_circle"
+        value="true"
+      >
+        <v-list-tile slot="activator">
+          <v-list-tile-title>Item2</v-list-tile-title>
+        </v-list-tile>
+        <v-list-group
+          sub-group
+          no-action
+          value="true"
+        >
+          <v-list-tile slot="activator">
+            <v-list-tile-title>sub-1</v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile
+            v-for="(admin, i) in admins"
+            :key="i"
+          >
+            <v-list-tile-title v-text="admin[0]"></v-list-tile-title>
+            <v-list-tile-action>
+              <v-icon v-text="admin[1]"></v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
+        <v-list-group
+          sub-group
+          no-action
+        >
+          <v-list-tile slot="activator">
+            <v-list-tile-title>sub-2</v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile
+            v-for="(crud, i) in cruds"
+            :key="i"
+          >
+            <v-list-tile-title v-text="crud[0]"></v-list-tile-title>
+            <v-list-tile-action>
+              <v-icon v-text="crud[1]"></v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
+      </v-list-group>
+    </v-list>
+  </v-navigation-drawer>
+		 </v-flex>	
+	 </v-layout>	 
+ </v-container>
+</template>
+<style lang="scss" scoped>
+  .height {
+		height: 100vh !important;
+	}
+
+</style>
