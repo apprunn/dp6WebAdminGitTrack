202872b3e67c523fa214d2b1b328246f8afcc1d4
elenaJO <ele.lucero1993@gmail.com>
2018-05-28 09:45:29 -0500

    maquetacion formulrio template

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
 import login from '@/components/login';
+import templates from '@/components/templates';
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
 			path: '/login',
 			name: 'login',
 			component: login,
+		},
+		{
+			path: '/templates/new',
+			name: 'templates',
+			component: templates,
 		},
 	],
 });
