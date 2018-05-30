6442c7fa20accb8082a940f9771e53bb03694781
elenaJO <ele.lucero1993@gmail.com>
2018-05-30 16:51:49 -0500

    avance

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,3 @@
-
 <template>
  	<v-container :fluid="true" pl-0 pt-0 pr-0 pb-0>
 	 	<v-layout row wrap>
@@ -10,24 +9,10 @@
 								<img class="icon" :src="result.pathIcon">
           			<v-list-tile-title>{{result.name}}</v-list-tile-title>
         			</v-list-tile>
-							<div v-for="resultlist in result.resource.data" :key="resultlist.position">
-								<div  v-if="option(resultlist.name)">
-									<v-list-tile v-for="resultDev in resultsDeveloper" :key="resultDev.userId">
-										 <v-list-tile-avatar>
-                         <img :src="resultDev.avatarUrl">
-                    </v-list-tile-avatar>
-										 <v-list-tile-content>
-                        <v-list-tile-title>{{resultDev.developerName}}</v-list-tile-title>
-                     </v-list-tile-content>
-									</v-list-tile>
-								</div>
-								<div v-else>
-									<v-list-tile v-for="resultlist in result.resource.data" :key="resultlist.position">
-            				<v-list-tile-title class="sub-item">{{resultlist.name}}</v-list-tile-title>
-	              		<img :src="resultlist.iconUrl" class="icon">
-									</v-list-tile>
-								</div>
-							</div>
+       				<v-list-tile v-for="resultlist in result.resource.data" :key="resultlist.position">
+            		<v-list-tile-title class="sub-item" @click="option(resultlist.name)">{{resultlist.name}}</v-list-tile-title>
+	              <img :src="resultlist.iconUrl" class="icon">
+							</v-list-tile>
       			</v-list-group>
     			</v-list>
   			</v-navigation-drawer>
@@ -40,26 +25,22 @@
 
 async function created() {
 	const url = 'sidebar/git';
-	const urlDeveloper = 'developer';
 	const token = localStorage.getItem('token');
 	const response = await this.$httpAcl.get(url, {
 		headers: { Authorization: `Bearer ${token}` },
 	});
 	this.results = response.data.data;
-
-	const responseDeveloper = await this.$http.get(urlDeveloper, {
-		headers: { Authorization: `Bearer ${token}` },
-	});
-	this.resultsDeveloper = responseDeveloper.data;
 }
 
 function option(value) {
-	return value === 'Lista de desarrolladores';
+	if (value === 'Lista de desarrolladores') {
+		this.$router.push({ name: 'developers' });
+	}
 }
+
 function data() {
 	return {
 		results: [],
-		resultsDeveloper: [],
 	};
 }
 export default {
---
+++ src/components/developers.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+				<v-list-tile v-for="resultDev in resultsDeveloper" :key="resultDev.userId">
+					<v-list-tile-avatar>
+            <img :src="resultDev.avatarUrl">
+          </v-list-tile-avatar>
+					<v-list-tile-content>
+            <v-list-tile-title>{{resultDev.developerName}}</v-list-tile-title>
+          </v-list-tile-content>
+				</v-list-tile>
+    </v-flex>
+  </v-layout>
+</template>
+
+
+<script>
+
+async function created() {
+	const urlDeveloper = 'developer';
+	const token = localStorage.getItem('token');
+	const responseDeveloper = await this.$http.get(urlDeveloper, {
+		headers: { Authorization: `Bearer ${token}` },
+	});
+	this.resultsDeveloper = responseDeveloper.data;
+	console.log(this.resultsDeveloper);
+}
+
+function data() {
+	return {
+		resultsDeveloper: [],
+	};
+}
+export default {
+	data,
+	created,
+};
+</script>
