7b0b4912f19ae0ecd80a29da279559d15d033ee0
elenaJO <ele.lucero1993@gmail.com>
2018-05-30 14:59:49 -0500

    avanece

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,15 @@
 <template>
  	<v-container :fluid="true" pl-0 pt-0 pr-0 pb-0>
+		<v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+    </v-toolbar>
 	 	<v-layout row wrap>
 		 	<v-flex xs8 md3>
 			 <v-navigation-drawer stateless value="true" class="blue lighten-3 height">
