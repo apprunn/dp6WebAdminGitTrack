4551a03db52ca50b8290f08647cc86b894d4decb
elenaJO <ele.lucero1993@gmail.com>
2018-05-30 16:00:25 -0500

    Agregar toolbar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,16 @@
 <template>
  	<v-container :fluid="true" pl-0 pt-0 pr-0 pb-0>
 		<v-toolbar>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>Link One</v-btn>
-      <v-btn flat>Link Two</v-btn>
-      <v-btn flat>Link Three</v-btn>
+      <v-toolbar-items>
+      <v-btn flat>{{codigo}}</v-btn>
     </v-toolbar-items>
     </v-toolbar>
-	 	<v-layout row wrap>
+		<div>
+			<v-layout row wrap>
 		 	<v-flex xs8 md3>
-			 <v-navigation-drawer stateless value="true" class="blue lighten-3 height">
+			 <v-navigation-drawer stateless value="true" class="blue lighten-3 height"  v-model="drawer">
     			<v-list v-for="result in results" :key="result.id">
 			 			<v-list-group value="true">
         			<v-list-tile slot="activator">
@@ -28,6 +26,7 @@
   			</v-navigation-drawer>
 		 	</v-flex>	
 	 	</v-layout>	 
+		</div> 
  </v-container>
 </template>
 
@@ -44,6 +43,8 @@ async function created() {
 function data() {
 	return {
 		results: [],
+		drawer: null,
+		codigo: localStorage.getItem('codigo'),
 	};
 }
 export default {
