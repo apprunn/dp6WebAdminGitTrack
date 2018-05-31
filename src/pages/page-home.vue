6f23b6fd8dc3e284e3b427d22d1be72715235618
elenaJO <ele.lucero1993@gmail.com>
2018-05-31 17:27:29 -0500

    avance

--- src/pages/page-home.vue
+++ src/pages/page-home.vue
@@ -1,12 +1,42 @@
 <template>
  	<layout-admin>
+		  <v-breadcrumbs divider="/">
+			<v-breadcrumbs-item to="/" router>Home</v-breadcrumbs-item>
+					<v-breadcrumbs-item 
+         v-for="item in array"
+				 :key="item"
+				 :to="item"
+			>
+        {{ item }}
+      </v-breadcrumbs-item>
+    </v-breadcrumbs>
 		 <router-view></router-view>
  	</layout-admin>
 </template>
 
 <script>
 
+
+function ruta() {
+	return this.$route.path;
+}
+
+function routes() {
+	this.array = this.$route.path.slice(1).split('/');
+}
+
+function data() {
+	return {
+		array: [],
+	};
+}
+
 export default {
 	name: 'page-home',
+	data,
+	computed: {
+		ruta,
+		routes,
+	},
 };
 </script>
