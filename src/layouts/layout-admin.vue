f65e2d05f51bc1f199c85bccff2ef3941916610b
elenaJO <ele.lucero1993@gmail.com>
2018-05-31 13:10:19 -0500

    cambiando nombre a la variable

--- src/layouts/layout-admin.vue
+++ src/layouts/layout-admin.vue
@@ -4,7 +4,7 @@
 				<v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
 				<v-spacer></v-spacer>
 					<v-toolbar-items>
-						<v-btn flat>{{codigo}}</v-btn>
+						<v-btn flat>{{codeUser}}</v-btn>
 					</v-toolbar-items>
     	</v-toolbar>
 	 	<v-layout row wrap>
@@ -50,7 +50,7 @@ function data() {
 	return {
 		results: [],
 		drawer: null,
-		codigo: localStorage.getItem('codigo'),
+		codeUser: localStorage.getItem('code-user'),
 	};
 }
 export default {
