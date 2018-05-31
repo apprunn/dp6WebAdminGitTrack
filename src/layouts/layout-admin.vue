03e9dd300ca51ebd0a18f80dcce82d4424eef30c
elenaJO <ele.lucero1993@gmail.com>
2018-05-31 18:31:22 -0500

    correciones orden e identacion

--- src/layouts/layout-admin.vue
+++ src/layouts/layout-admin.vue
@@ -1,26 +1,31 @@
 <template>
   <v-container :fluid="true" pl-0 pt-0 pr-0 pb-0>
-       <v-toolbar>
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-spacer></v-spacer>
-          <v-toolbar-items>
-            <v-btn flat>{{codeUser}}</v-btn>
-          </v-toolbar-items>
-      </v-toolbar>
-    	<v-navigation-drawer v-model="drawer" temporary absolute class="blue lighten-3">
-				<v-list v-for="result in results" :key="result.id">
-							<v-list-group value="true">
-								<v-list-tile slot="activator">
-									<img class="icon" :src="result.pathIcon">
-									<v-list-tile-title>{{result.name}}</v-list-tile-title>
-								</v-list-tile>
-								<v-list-tile v-for="resultlist in result.resource.data" :key="resultlist.position">
-									<v-list-tile-title class="sub-item" @click="option(resultlist.name)">{{resultlist.name}}</v-list-tile-title>
-									<img :src="resultlist.iconUrl" class="icon">
-								</v-list-tile>
-							</v-list-group>
-					</v-list>
-    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>{{codeUser}}</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer 
+			v-model="drawer" 
+			temporary
+			absolute
+			class="blue lighten-3"
+		>
+			<v-list v-for="result in results" :key="result.id">
+				<v-list-group value="true">
+					<v-list-tile slot="activator">
+						<img class="icon" :src="result.pathIcon">
+						<v-list-tile-title>{{result.name}}</v-list-tile-title>
+					</v-list-tile>
+					<v-list-tile v-for="resultlist in result.resource.data" :key="resultlist.position">
+						<v-list-tile-title class="sub-item" @click="option(resultlist.name)">{{resultlist.name}}</v-list-tile-title>
+						<img :src="resultlist.iconUrl" class="icon">
+					 </v-list-tile>
+					</v-list-group>
+			</v-list>
+     </v-navigation-drawer>
 		<slot></slot>
   </v-container>
 </template>
