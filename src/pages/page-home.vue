81ffcb3d22c493458cd8093d635d4ab03acb66ff
elenaJO <ele.lucero1993@gmail.com>
2018-06-01 19:06:13 -0500

    avance

--- src/pages/page-home.vue
+++ src/pages/page-home.vue
@@ -2,13 +2,13 @@
  	<layout-admin>
 		 {{routes}}
 		  <v-breadcrumbs divider="/">
-			<v-breadcrumbs-item to="/">Home</v-breadcrumbs-item>
 			<v-breadcrumbs-item 
-         v-for="element in arrayRoutes"
-				 :key="element"
-				 :to="element"
+         v-for="element in route"
+				 :key="element.name"
+				 :to="element.link"
+				 :disabled="false"
 			>
-        {{ element }}
+        {{ element.name }}
       </v-breadcrumbs-item>
     </v-breadcrumbs>
 		 <router-view></router-view>
@@ -17,12 +17,18 @@
 
 <script>
 function routes() {
+	this.route = this.$route.meta.breadcrumb;
 	this.arrayRoutes = this.$route.path.slice(1).split('/');
+	this.arrayRoutes2 = this.$route.path.split('/');
+	console.log(this.route);
 }
 
 function data() {
 	return {
 		arrayRoutes: [],
+		arrayRoutes2: [],
+		bread: [],
+		route: null,
 	};
 }
 
--- src/router/index.js
+++ src/router/index.js
@@ -20,18 +20,41 @@ export default new Router({
 					path: '/developers',
 					name: 'developers',
 					component: pageDevelopers,
+					meta: {
+						breadcrumb: [
+							{ name: 'Home', link: '/' },
+							{ name: 'Developers', link: '/developers' },
+						],
+					},
 				},
 				{
 					path: '/projects',
 					name: 'projects',
 					component: pageProjects,
+					meta: {
+						breadcrumb: [
+							{ name: 'Home', link: '/' },
+							{ name: 'Project', link: '/projects' },
+						],
+					},
 				},
 				{
 					path: '/projects/:id/activity',
 					name: 'activity',
 					component: pageActivity,
+					meta: {
+						breadcrumb: [
+							{ name: 'Home', link: '/' },
+							{ name: 'Project', link: '/projects' },
+						],
+					},
 				},
 			],
+			meta: {
+				breadcrumb: [
+					{ name: 'Home', link: '/' },
+				],
+			},
 		},
 		{
 			path: '/login',
